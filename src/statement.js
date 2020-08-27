fc571884304089912195cc8a79735a213ff2eefa
SUNSK <senkk512@163.com>
2020-08-27 15:18:41 +0800

    [exercise][Sky] refactor: delete extra annotation

--- src/statement.js
+++ src/statement.js
@@ -32,11 +32,8 @@ function statement (invoice, plays) {
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = calculateThisAmount(play, perf);
-    // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    //print line for this order
     result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
