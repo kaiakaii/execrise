d3b5b6e889d320b61475233538247ea280b1c988
SUNSK <senkk512@163.com>
2020-08-27 15:23:18 +0800

    [exercise][Sky]refactor: extract getFormat function

--- src/statement.js
+++ src/statement.js
@@ -20,15 +20,19 @@ function calculateThisAmount(play, perf) {
   return thisAmount;
 }
 
-function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
+function getFormat() {
+  return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
   }).format;
+}
+
+function statement (invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = getFormat();
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = calculateThisAmount(play, perf);
@@ -37,8 +41,7 @@ function statement (invoice, plays) {
     result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits \n`;
+  result += `Amount owed is ${format(totalAmount / 100)}\nYou earned ${volumeCredits} credits \n`;
   return result;
 }
 
