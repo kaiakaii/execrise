d8c2f71fe193763962436b31d9194c51517f4d84
SUNSK <senkk512@163.com>
2020-08-27 16:55:20 +0800

    [exercise][Sky]refactor:implement statementHtml method

--- src/statement.js
+++ src/statement.js
@@ -34,25 +34,48 @@ function getVolumeCredits(volumeCredits, perf, play) {
   return volumeCredits;
 }
 
+function getResultMessage(plays, perf, volumeCredits, totalAmount) {
+  const play = plays[perf.playID];
+  let thisAmount = calculateThisAmount(play, perf);
+  volumeCredits = getVolumeCredits(volumeCredits, perf, play);
+  totalAmount += thisAmount;
+  return {play, thisAmount, volumeCredits, totalAmount};
+}
+
 function generateText(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   let totalAmount = 0;
   let volumeCredits = 0;
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = calculateThisAmount(play, perf);
-    volumeCredits = getVolumeCredits(volumeCredits, perf, play);
-    result += ` ${play.name}: ${getFormat()(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    const resultMessage = getResultMessage(plays, perf, volumeCredits, totalAmount);
+    volumeCredits = resultMessage.volumeCredits;
+    totalAmount = resultMessage.totalAmount;
+    result += ` ${resultMessage.play.name}: ${getFormat()(resultMessage.thisAmount / 100)} (${perf.audience} seats)\n`;
   }
   result += `Amount owed is ${getFormat()(totalAmount / 100)}\nYou earned ${volumeCredits} credits \n`;
+  return result;
+}
+function generateHTML(invoice, plays) {
+  let result = `<h1>Statement for ${invoice.customer}</h1>\n<table>\n<tr><th>play</th><th>seats</th><th>cost</th></tr>`;
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    const resultMessage = getResultMessage(plays, perf, volumeCredits, totalAmount);
+    volumeCredits = resultMessage.volumeCredits;
+    totalAmount = resultMessage.totalAmount;
+    result += ` <tr><td>${resultMessage.play.name}</td><td>${perf.audience}</td><td>${getFormat()(resultMessage.thisAmount / 100)}</td></tr>\n`;
+  }
+  result += `</table>\n<p>Amount owed is <em>${getFormat()(totalAmount / 100)}</em></p>\n<p>You earned <em>${volumeCredits}</em> credits</p>\n`;
   return result;
 }
 
 function statement (invoice, plays) {
   return generateText(invoice, plays);
 }
+function statementHtml (invoice, plays) {
+  return generateHTML(invoice, plays);
+}
 
 module.exports = {
-  statement,
+  statement,statementHtml
 };
